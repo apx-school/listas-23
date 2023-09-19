75bde484e45a82ac9081ec0b4df8ec1fcd2f576c
Marcelo Zapaia <zapaiamarce@gmail.com>
2023-09-19 15:48:30 -0300

    fix get token

--- src/pages/api/test/handler.ts
+++ src/pages/api/test/handler.ts
@@ -1,10 +1,8 @@
-import { getToken } from "lib/auth";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const newCode = await getToken("marce@apx.school");
-  res.status(200).json({ newCode });
+  res.status(200).json({});
 }
